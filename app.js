79830bdafef9c1685c8367e81bab0c4e9f35b52d
onedayl <onedayl@Daisy>
2019-10-18 18:08:42 +0800

    first commit

---
+++ app.js
@@ -0,0 +1,22 @@
+const Koa = require('koa')
+const Router = require('@koa/router')
+const bodyParser = require('koa-bodyparser')
+
+const app = new Koa()
+const router = new Router()
+
+router
+  .get('/', ctx => {
+    ctx.body = 'index'
+  })
+  .post('/message', ctx => {
+    console.log(ctx.request.body)
+    ctx.body = 'message'
+}) 
+
+app.use(bodyParser())
+app.use(router.routes())
+app.use(router.allowedMethods())
+
+app.listen(5000);
+console.log('Listening on port 5000')
